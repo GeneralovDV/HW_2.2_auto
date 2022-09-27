a9776e414ca0275c3f9d16c278bb1f5548507edc
Дмитрий Генералов <generalovdmitriypsk@gmail.com>
2022-09-27 21:34:11 +0300

    edited task

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -13,21 +14,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
-    String meetingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    public String meetingDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
 
     @Test
     void shouldSentFormPopularName() {
-//        Configuration.holdBrowserOpen=true;
+        String planningDate = meetingDate(5);
         open("http://localhost:9999");
         $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
-        $("[placeholder=\"Дата встречи\"]").setValue(meetingDate);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[name=\"name\"]").setValue("Иванов Иван");
         $("[name=\"phone\"]").setValue("+79111111111");
         $("[data-test-id=\"agreement\"]").click();
         $x("//span[text()=\"Забронировать\"]").click();
         $x("//div[@class=\"notification__title\"]").shouldBe(visible, Duration.ofSeconds(15));//задержка
-        $("[data-test-id=notification] .notification__content").should(exactText("Встреча успешно забронирована на " + meetingDate));
+        $("[data-test-id=notification] .notification__content").should(exactText("Встреча успешно забронирована на " + planningDate));
     }
 
 
